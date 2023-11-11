042bd6cfbc5ba7f12083f5e0bcee77b61a093fef
Biswajeet Sahoo <2000biswajeet@gmail.com>
2023-11-11 19:56:51 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shoes_app_ui/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Averta'
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/widgets/product_slider.dart
@@ -0,0 +1,109 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:shoes_app_ui/theme/colors.dart';
+
+class ActiveDot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Container(
+        width: 20,
+        height: 5,
+        decoration: BoxDecoration(
+          color: black,
+          borderRadius: BorderRadius.circular(5),
+        ),
+      ),
+    );
+  }
+}
+
+class InactiveDot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Container(
+        width: 8,
+        height: 5,
+        decoration: BoxDecoration(
+          color: black.withOpacity(0.3),
+          borderRadius: BorderRadius.circular(5),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+ * for slider home page
+ */
+class ProductSlider extends StatefulWidget {
+  final List<String>? items;
+
+  ProductSlider({this.items});
+
+  @override
+  _ProductSliderState createState() =>
+      _ProductSliderState();
+}
+
+class _ProductSliderState extends State<ProductSlider> {
+  int activeIndex = 0;
+  setActiveDot(index) {
+    setState(() {
+      activeIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: Column(
+        // overflow: Overflow.visible,
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: MediaQuery.of(context).size.width,
+            child: CarouselSlider(
+              items: widget.items!.map((item) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Stack(
+                      children: <Widget>[
+                        Container(
+                          
+                          width: MediaQuery.of(context).size.width,
+                          child: Image(
+                            image: AssetImage("assets/images/"+item),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        
+                      ],
+                    );
+                  },
+                );
+              }).toList(),
+              options: CarouselOptions(
+                onPageChanged: (index, _) {
+                  setActiveDot(index);
+                },
+                autoPlayCurve: Curves.fastLinearToSlowEaseIn,
+                autoPlayAnimationDuration: const Duration(seconds: 2),
+                viewportFraction: 1.0,
+              ),
+            ),
+          ),
+          Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(widget.items!.length, (idx) {
+                  return activeIndex == idx ? ActiveDot() : InactiveDot();
+                }))
+        ],
+      ),
+    );
+  }
+}
